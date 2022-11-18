8f38c440e41d6b132490147a22eb51a2d44cf85f
kongweirui <weiruik@outlook.com>
2022-11-18 09:43:46 +0800

    Initial Commit

---
+++ ServerStatus/ServerStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServerStatusApp.swift
+//  ServerStatus
+//
+//  Created by 孔维锐 on 2022/11/18.
+//
+
+import SwiftUI
+
+@main
+struct ServerStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
